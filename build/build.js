3226d4bf7b84fe5e6748a018eba60219d4b5422e
TheMezStrikes <=>
2020-09-27 14:50:49 +0200

    Rewrite header

--- build/build.js
+++ build/build.js
@@ -69,31 +69,33 @@ let headers = {
 }
 
 let header = `                        <!-- Header is loaded in build/build.js and loaded in src/header.js-->
-        <a href="/pokeseed/index.html" class="logo">    <!--Logo functions as link to home page. Add actual link for clarity later?-->
+        <a href="/pokeseed/index.html" class="subtle-link logo">    <!--Logo functions as link to home page. Add actual link for clarity later?-->
             <img src="/pokeseed/img/pokeseed.png">              
             <h1>PokeSeed</h1>
         </a>
         <div id="menu-button"></div>
-        <nav id="navigation">\r\n`;
+        <nav id="navigation">
+            <ul class="menu">\r\n`;
 
 let tab = `    `;
 
 
 for (let dirname in headers) {
-    header += tab+tab+tab+`<div class="dropdown">\r\n`;
-    header += tab+tab+tab+tab+`<p>${dirname}</p>\r\n`;
-    header += tab+tab+tab+tab+`<div class="dropdown-content">\r\n`
+    header += tab+tab+tab+tab+`<li class="dropdown">\r\n`;
+    header += tab+tab+tab+tab+tab+`<p>${dirname}</p>\r\n`;
+    header += tab+tab+tab+tab+tab+`<ul class="dropdown-content">\r\n`
     let folders = headers[dirname];
     for (let name of folders) {
         let exists = FS.existsSync(root + `${toPageId(dirname)}`) && FS.existsSync(root + `${toPageId(dirname)}/${toPageId(name)}.html`);
         let href = exists ? `/pokeseed/html/${toPageId(dirname)}/${toPageId(name)}.html` : "";
-        header += tab+tab+tab+tab+tab+`<a href="${href}"${exists ? '' : ' class="disabled"'}>\r\n`;
-        header += tab+tab+tab+tab+tab+tab+`${name}\r\n`;
-        header += tab+tab+tab+tab+tab+`</a>\r\n`;
+        header += tab+tab+tab+tab+tab+tab+`<a href="${href}"${exists ? '' : ' class="disabled"'}>\r\n`;
+        header += tab+tab+tab+tab+tab+tab+tab+`<li>${name}</li>\r\n`;
+        header += tab+tab+tab+tab+tab+tab+`</a>\r\n`;
     }
-    header += tab+tab+tab+tab+`</div>\r\n`
-    header += tab+tab+tab+`</div>\r\n`;
+    header += tab+tab+tab+tab+tab+`</ul>\r\n`
+    header += tab+tab+tab+tab+`</li>\r\n`;
 }
+header += tab+tab+tab+`</ul>\r\n`;
 header += tab+tab+`</nav>\r\n`;
 header += tab;
 
@@ -102,3 +104,36 @@ let text = FS.readFileSync('../src/header.js', 'utf8');
 console.log(text.match(HeaderMatch));
 text = text.replace(HeaderMatch, "~\nvar headerhtml = `" + header + "`\n// ~");
 FS.writeFileSync('../src/header.js', text);
+
+/*<nav id="navigation">
+<ul class="menu">
+    <a href="./index.html">
+        <li>Home</li>
+    </a>
+    <a href="./images.html">
+        <li>Images</li>
+    </a>
+    <li class="dropdown">
+        <p>Text Pages</p>
+        <ul class="dropdown-content">
+            <a href="./text-1.html">
+                <li>Text 1</li>
+            </a>
+            <a href="./text-1.html">
+                <li>Text 2</li>
+            </a>
+        </ul>
+    </li>
+    <li class="dropdown">
+        <p>About Us</p>
+        <ul class="dropdown-content">
+            <a href="./contact.html">
+                <li>Contact</li>
+            </a>
+            <a href="./about-us.html">
+                <li>Meet the Team</li>
+            </a>
+        </ul>
+    </li>
+</ul>
+</nav>*/
--- src/header.js
+++ src/header.js
@@ -1,4 +1,4 @@
-var HeaderMatch = new RegExp(`<header class="menu navbar">(.|\n|\r)*(?=<\/header>)`, 'gmi');
+var HeaderMatch = new RegExp(`<header class="navbar">(.|\n|\r)*(?=<\/header>)`, 'gmi');
 
 var toId = function (str) {
     return '' + str.replace(/[^0-9a-z]/gi, '').toLowerCase()
@@ -6,178 +6,180 @@ var toId = function (str) {
 
 // ~
 var headerhtml = `                        <!-- Header is loaded in build/build.js and loaded in src/header.js-->
-        <a href="/pokeseed/index.html" class="logo">    <!--Logo functions as link to home page. Add actual link for clarity later?-->
+        <a href="/pokeseed/index.html" class="subtle-link logo">    <!--Logo functions as link to home page. Add actual link for clarity later?-->
             <img src="/pokeseed/img/pokeseed.png">              
             <h1>PokeSeed</h1>
         </a>
         <div id="menu-button"></div>
         <nav id="navigation">
-            <div class="dropdown">
-                <p>Gen1</p>
-                <div class="dropdown-content">
-                    <a href="/pokeseed/html/gen1/red.html">
-                        Red
-                    </a>
-                    <a href="" class="disabled">
-                        Blue/Green
-                    </a>
-                    <a href="" class="disabled">
-                        Yellow
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen2</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        Gold
-                    </a>
-                    <a href="" class="disabled">
-                        Silver
-                    </a>
-                    <a href="" class="disabled">
-                        Crystal
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen3</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        Ruby
-                    </a>
-                    <a href="" class="disabled">
-                        Sapphire
-                    </a>
-                    <a href="/pokeseed/html/gen3/emerald.html">
-                        Emerald
-                    </a>
-                    <a href="/pokeseed/html/gen3/fire-red.html">
-                        Fire Red
-                    </a>
-                    <a href="/pokeseed/html/gen3/leaf-green.html">
-                        Leaf Green
-                    </a>
-                    <a href="" class="disabled">
-                        Colosseum
-                    </a>
-                    <a href="" class="disabled">
-                        Gale of Darkness
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen4</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        Pearl
-                    </a>
-                    <a href="" class="disabled">
-                        Diamond
-                    </a>
-                    <a href="" class="disabled">
-                        Platinum
-                    </a>
-                    <a href="" class="disabled">
-                        Heart Gold
-                    </a>
-                    <a href="" class="disabled">
-                        Soul Silver
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen5</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        Black
-                    </a>
-                    <a href="" class="disabled">
-                        White
-                    </a>
-                    <a href="" class="disabled">
-                        Black 2
-                    </a>
-                    <a href="" class="disabled">
-                        White 2
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen6</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        X
-                    </a>
-                    <a href="" class="disabled">
-                        Y
-                    </a>
-                    <a href="" class="disabled">
-                        Omega Ruby
-                    </a>
-                    <a href="" class="disabled">
-                        Alpha Sapphire
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen7</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        Sun
-                    </a>
-                    <a href="" class="disabled">
-                        Moon
-                    </a>
-                    <a href="" class="disabled">
-                        Ultra Sun
-                    </a>
-                    <a href="" class="disabled">
-                        Ultra Moon
-                    </a>
-                    <a href="" class="disabled">
-                        Let's Go Pikachu
-                    </a>
-                    <a href="" class="disabled">
-                        Let's Go Eevee
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Gen8</p>
-                <div class="dropdown-content">
-                    <a href="" class="disabled">
-                        Sword
-                    </a>
-                    <a href="" class="disabled">
-                        Shield
-                    </a>
-                    <a href="" class="disabled">
-                        DLC Sword
-                    </a>
-                    <a href="" class="disabled">
-                        DLC Shield
-                    </a>
-                </div>
-            </div>
-            <div class="dropdown">
-                <p>Other</p>
-                <div class="dropdown-content">
-                    <a href="/pokeseed/html/other/type-coverage.html">
-                        Type Coverage
-                    </a>
-                    <a href="" class="disabled">
-                        Randomizer
-                    </a>
-                    <a href="/pokeseed/html/other/about-us.html">
-                        About Us
-                    </a>
-                </div>
-            </div>
+            <ul class="menu">
+                <li class="dropdown">
+                    <p>Gen1</p>
+                    <ul class="dropdown-content">
+                        <a href="/pokeseed/html/gen1/red.html">
+                            <li>Red</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Blue/Green</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Yellow</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen2</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>Gold</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Silver</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Crystal</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen3</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>Ruby</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Sapphire</li>
+                        </a>
+                        <a href="/pokeseed/html/gen3/emerald.html">
+                            <li>Emerald</li>
+                        </a>
+                        <a href="/pokeseed/html/gen3/fire-red.html">
+                            <li>Fire Red</li>
+                        </a>
+                        <a href="/pokeseed/html/gen3/leaf-green.html">
+                            <li>Leaf Green</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Colosseum</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Gale of Darkness</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen4</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>Pearl</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Diamond</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Platinum</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Heart Gold</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Soul Silver</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen5</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>Black</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>White</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Black 2</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>White 2</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen6</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>X</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Y</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Omega Ruby</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Alpha Sapphire</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen7</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>Sun</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Moon</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Ultra Sun</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Ultra Moon</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Let's Go Pikachu</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Let's Go Eevee</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Gen8</p>
+                    <ul class="dropdown-content">
+                        <a href="" class="disabled">
+                            <li>Sword</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Shield</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>DLC Sword</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>DLC Shield</li>
+                        </a>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <p>Other</p>
+                    <ul class="dropdown-content">
+                        <a href="/pokeseed/html/other/type-coverage.html">
+                            <li>Type Coverage</li>
+                        </a>
+                        <a href="" class="disabled">
+                            <li>Randomizer</li>
+                        </a>
+                        <a href="/pokeseed/html/other/about-us.html">
+                            <li>About Us</li>
+                        </a>
+                    </ul>
+                </li>
+            </ul>
         </nav>
     `
 // ~
 
 jQuery(function() {
-    document.getElementsByClassName("menu navbar")[0].innerHTML = headerhtml;
+    document.getElementsByClassName("navbar")[0].innerHTML = headerhtml;
 })
